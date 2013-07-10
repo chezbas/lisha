704cfc1203a3b18445f49ac22711c7353468dcec
chezbas <chezbas@gmail.com>
2013-07-10 16:23:34 +0200

    Fix language directive in URL

--- lisha1.00/includes/common/language.php
+++ lisha1.00/includes/common/language.php
@@ -1,22 +1,22 @@
 <?php
 	// Get the key name of language directive
 	$a_valeur_interdite = explode('|',$_SESSION[$ssid]['lisha']['configuration'][2]);
-	
-	if(!isset($_GET[$a_valeur_interdite[1]]))
+    error_log(print_r($a_valeur_interdite,true));
+	if(!isset($_GET[$a_valeur_interdite[4]]))
 	{	
 		$_SESSION[$ssid]['lisha']['langue'] = $_SESSION[$ssid]['lisha']['configuration'][1];
 	}
 	else
 	{
-		$fi_lng = $link->real_escape_string($_GET[$a_valeur_interdite[1]]);
-		
-		$query = 'SELECT
+		$fi_lng = $link->real_escape_string($_GET[$a_valeur_interdite[4]]);
+
+        $query = 'SELECT
 					1  
 				FROM 
-					`'.$_SESSION[$ssid]['lisha']['struct']['tb_lang']['name'].'`
+					`'.__LISHA_TABLE_LANGUAGE__.'`
 				WHERE 1 = 1
 					AND `id` = "'.$fi_lng.'"';
-
+        error_log($query);
 		$result = $link->query($query);
 		
 		if(mysqli_num_rows($result) == 1)
@@ -30,4 +30,3 @@
 	}
 
 	$_GET['lng'] = $_SESSION[$ssid]['lisha']['langue'];
-?>
